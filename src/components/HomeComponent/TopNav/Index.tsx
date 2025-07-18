ee7369f89285fd06809b24d9c337970eb24f618f
Siyam hosan <Siyam.hosan@yahoo.com>
2025-07-19 00:15:42 +0600

    Refactor TopNav component to use icons from IconsProvider and improve layout with flexbox

--- src/components/HomeComponent/TopNav/Index.tsx
+++ src/components/HomeComponent/TopNav/Index.tsx
@@ -1,33 +1,43 @@
-import React, { useState, type JSX } from "react";
+import type React from "react";
+import { useState } from "react";
+import { icons } from "../../../helpers/IconsProvider";
 import Container from "../../commonComponent/Container";
-import {
-  reddit,
-  youtube,
-  instagram,
-  pinterest,
-  facebook,
-  twitter,
-} from "../../../helpers/IconsProvider";
 
 const TopNav = () => {
-  const [topNav] = useState<Array[JSX.Element]>([
-    reddit,
-    youtube,
-    instagram,
-    pinterest,
-    facebook,
-    twitter,
+  const [topNav] = useState<Array<React.ReactNode>>([
+    icons.reddit,
+    icons.youtube,
+    icons.instagram,
+    icons.pinterest,
+    icons.facebook,
+    icons.twitter,
   ]);
-  return (
-    <div className="bg-secondary-700 py-5!">
-      <Container>
-        <div className="grid grid-cols-2 items-center">
-          <h1 className="body-small-400 text-gray-00">
-            Welcome to Clicon online eCommerce store.{" "}
-          </h1>
-        </div>
 
-        <div className="body-small-400 text-gray-00">Follow Us</div>
+  return (
+    <div className="bg-secondary-700 py-3 h-12 flex items-center">
+      <Container>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-2">
+            <h1 className="text-gray-100 text-sm hover:text-primary-500 transition-colors cursor-pointer">
+              Welcome to Clicon online eCommerce store
+            </h1>
+          </div>
+
+          <div className="flex items-center gap-6">
+            <span className="text-gray-100 text-sm font-medium">Follow Us</span>
+            <div className="flex items-center gap-4">
+              {topNav.map((icon, index) => (
+                <button
+                  type="button"
+                  key={index.toString()}
+                  className="text-gray-300 hover:text-primary-500 transition-colors text-lg"
+                >
+                  {icon}
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   );
